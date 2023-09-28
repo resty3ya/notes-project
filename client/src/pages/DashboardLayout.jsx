4727deb9d6900b62d4f400bb172547c1f5d07c5a
unknown <IT Admin@DESKTOP-12JBMGC.VIAMARE.COM>
2023-09-28 17:58:50 +0800

    update client module

---
+++ client/src/pages/DashboardLayout.jsx
@@ -0,0 +1,7 @@
+import Wrapper from "../assets/wrappers/Dashboard";
+import { toast } from "react-toastify";
+
+const DashboardLayout = () => {
+  return <Wrapper>DashboardLayout</Wrapper>;
+};
+export default DashboardLayout;
