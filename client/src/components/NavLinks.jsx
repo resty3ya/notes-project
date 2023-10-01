eced49927b3d9e2504c490c122cca8124afd968a
resty3ya <resty3ya010@gmail.com>
2023-10-01 11:24:13 +0800

    ongoing working with navbar

--- client/src/components/NavLinks.jsx
+++ client/src/components/NavLinks.jsx
@@ -1,16 +1,18 @@
 import links from "../utils/links";
 import { NavLink } from "react-router-dom";
 
-const NavLinks = ({ text, path }) => {
+const NavLinks = () => {
   return (
     <div className="nav-links">
       {links.map((link) => {
         const { text, path } = link;
 
         return (
-          <NavLink to={path} key={text} className="nav-link" end>
-            {text}
-          </NavLink>
+          <div>
+            <NavLink to={path} key={text} className="nav-link" end>
+              {text}
+            </NavLink>
+          </div>
         );
       })}
     </div>
